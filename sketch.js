1ffeda0b65c2cf830b638721ad2f851b160dc089
Glauber Cavalcante <glauber_c@hotmail.com>
2022-09-22 13:12:32 -0400

    Project uploaded

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(500, 400);
+  somDaTrilha.loop();
+
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  movimentaAtor();
+  
+  mostraCarro(); 
+  movimentaCarro();
+  voltaPosicaoInicialCarro();
+  
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
+
+
